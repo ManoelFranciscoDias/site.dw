99a3d7c909dce84bf505558b03b0c5fdc88bd245
ManoelFranciscoDias <manoel2007dias@gmail.com>
2023-11-27 13:48:05 -0300

    comit

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 3){
+        cont = 1 
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
